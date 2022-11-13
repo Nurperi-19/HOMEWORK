af2a7f89163ad55009c28c1c79616b978f06475b
Nurperi-19 <nurperi.bakenova.97@gmail.com>
2022-11-13 17:30:41 +0600

    hw

---
+++ handlers/admin.py
@@ -0,0 +1,11 @@
+from aiogram import Dispatcher, types
+from config import dp, bot, ADMINS
+
+
+async def ban(message: types.Message):
+    if message.from_user.id not in ADMINS:
+        pass
+
+
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(ban, commands=['pin'], commands_prefix="!")
